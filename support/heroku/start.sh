8bef4c7a7a89e18d700541f48c29bddb8d03f865
Cody M Leff <codymleff@gmail.com>
2016-12-13 01:56:25 -0700

    Fixing error handling.

--- support/heroku/start.sh
+++ support/heroku/start.sh
@@ -4,5 +4,5 @@
 #   This script starts the Heroku server.
 #
 
-vendor/bin/heroku-php-nginx -C support/heroku/nginx.conf -p 8000 -v vendor/wordpress &
-node main.js
+vendor/bin/heroku-php-nginx -C support/heroku/nginx.conf -p 8000 -v vendor/wordpress 2>&1 &
+node main.js 2>&1
