a51c68580d215eb5b765ffc11f2c9f4d458a1968
Cody M Leff <codymleff@gmail.com>
2016-12-12 19:29:58 -0700

    Trying to make Heroku deploy work.

--- support/heroku/start.sh
+++ support/heroku/start.sh
@@ -4,7 +4,13 @@
 # This script starts the Heroku server.
 #
 
-if vendor/bin/heroku-php-nginx -C support/heroku/nginx.conf -p 8000 -v vendor/wordpress | grep -q 'Application ready for connections on port 8000.'; then
-    node main.js
-fi
-
+vendor/bin/heroku-php-nginx -C support/heroku/nginx.conf -p 8000 -v vendor/wordpress | {
+    while IFS= read -r line
+    do
+        if [[ $line == 'Application ready for connections on port 8000.' ]]
+        then
+            node main.js
+            break
+        fi
+    done
+}
