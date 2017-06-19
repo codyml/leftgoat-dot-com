32ccf5ba6d7c497ec1a0ab0375b466f3b43ffadf
Cody M Leff <codymleff@gmail.com>
2017-06-19 16:34:54 -0700

    Wrote Services section.

---
+++ leftgoat-custom-theme/parts/services.php
@@ -0,0 +1,6 @@
+<div class="above-services-banner" style="background-image: url('<?php echo get_field( 'services_full_width_image_above' ); ?>"');"></div>
+<section id="services">
+    <div class="fluid-container">
+        <?php echo get_field( 'services_content' ); ?>
+    </div>
+</section>
--- leftgoat-custom-theme/parts/who-we-are.php
+++ leftgoat-custom-theme/parts/who-we-are.php
@@ -1,5 +1,5 @@
 <section id="who-we-are">
-    <div class="fluid-container">
+    <article class="fluid-container">
         <?php echo get_field( 'who_we_are_content' ); ?>
-    </div>
+    </article>
 </section>
