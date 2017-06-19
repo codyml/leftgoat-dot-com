0203a44a923062679f8c82ddeaeb936f28903bb9
Cody M Leff <codymleff@gmail.com>
2017-06-19 16:15:02 -0700

    Cleaned up layout classes a bit.

--- leftgoat-custom-theme/parts/who-we-are.php
+++ leftgoat-custom-theme/parts/who-we-are.php
@@ -1,5 +1,5 @@
 <section id="who-we-are">
-    <div class="fluid-container body-content">
+    <div class="fluid-container">
         <?php echo get_field( 'who_we_are_content' ); ?>
     </div>
 </section>
