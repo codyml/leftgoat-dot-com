b02f549e9225f88797793d903d0ed09b3b69eafb
Cody M Leff <codymleff@gmail.com>
2017-06-20 18:12:28 -0700

    Cleaned up smooth scrolling code.

--- static-src/scripts/smooth-scrolling.js
+++ static-src/scripts/smooth-scrolling.js
@@ -6,15 +6,6 @@ import Velocity from 'velocity-animate'
 
 
 /*
-*   Constants
-*/
-
-const DURATION = 1000
-const EASING = 'ease-in-out'
-const LARGE_SCREEN_OFFSET = -110
-
-
-/*
 *   Enables smooth scrolling by intercepting clicks to anchor links,
 *   then animating the scroll to their position.
 */
@@ -27,44 +18,32 @@ export default () => {
         const anchor = anchorLink.href.match(/#.+$/)[0]
         anchorLink.addEventListener('click', event => {
 
-            console.log(anchorLink, event)
-
-            //  Prevents the normal, instantaneous scrolling
             event.preventDefault()
 
-            /*  Calculates whether the desired scroll position is below
-            *   the bottom of the document.  */
-            const target = document.querySelector(anchor)
+            /*  
+            *   Calculates whether the desired scroll position is below
+            *   the bottom of the document.
+            */
+
             const bodyHeight = document.body.scrollHeight
             const maxScrollPosition = bodyHeight - window.innerHeight
             
-            /*  Scrolls smoothly to either the top of the target element
-            *   or the bottom of the document.  */
-            if (target.offsetTop < maxScrollPosition) {
-
-                console.log('scrolling to: ', target, target.offsetTop, bodyHeight, maxScrollPosition)
-
-                Velocity(target, 'scroll', {
-
-                    offset: 0,
-                    duration: DURATION,
-                    easing: EASING,
-
-                })
-
-            } else {
-
-                console.log('scrolling to max scroll position: ', target, target.offsetTop, bodyHeight, maxScrollPosition)
-
-                Velocity(document.body, 'scroll', {
-
-                    offset: maxScrollPosition,
-                    duration: DURATION,
-                    easing: EASING,
-
-                })
-
+            let target = document.querySelector(anchor)
+            let offset = 0
+            const duration = 1250
+            const easing = 'ease-in-out'
+            
+            if (target.offsetTop > maxScrollPosition) {
+                offset = maxScrollPosition
+                target = document.body
             }
+
+            /*  
+            *   Scrolls smoothly to either the top of the target element
+            *   or the bottom of the document.
+            */
+
+            Velocity(target, 'scroll', { offset, duration, easing })
 
         })
 
